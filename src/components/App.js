450d78aee8445ead3e63be17aa2c2a63af1883d1
TravelFiend <mikegrace274@gmail.com>
2020-02-10 14:54:12 -0800

    basic header with routes in place

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './Header/Header';
+import CovrLettr from '../containers/CovrLettr';
+import AboutUs from '../containers/AboutUs';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Header />
+      {/* <Switch>
+        <Route exact path="/" component={CovrLettr} />
+        <Route exact path="/about" component={AboutUs} />
+      </Switch> */}
+    </Router>
+  );
 }
   
---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Header.css';
+import covrLettrImg from '../../assets/covrLettr.png';
+
+const Header = () => (
+  <header className={styles.Header}>
+    <img src={covrLettrImg} alt="logo" />
+    <nav>
+      <Link to="/">
+        <span>Home</span>
+      </Link>
+
+      <Link to="/about">
+        <span>About</span>
+      </Link>
+    </nav>
+  </header>
+);
+
+export default Header;
