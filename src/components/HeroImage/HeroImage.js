05ef8efc60b4da5eedbf11aaff0af8b9ed51f0f3
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2020-02-10 16:50:57 -0800

    Revert "Nikki branch"

--- src/components/HeroImage/HeroImage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import styles from './HeroImage.css';
-import HeroPicture from '../assets/heroImage.jpg';
-
-const HeroImage = () => <img className={styles.img} src={HeroPicture} />;
-
-export default HeroImage;
