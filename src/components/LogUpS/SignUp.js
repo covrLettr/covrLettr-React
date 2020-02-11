9d945d1db9ed233923b2a06e0c164a4d9dbbd871
TravelFiend <mikegrace274@gmail.com>
2020-02-11 14:53:06 -0800

    auth services written

--- src/components/LogUpS/SignUp.js
+++ src/components/LogUpS/SignUp.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import styles from './LogUpS.css';
 
-
-const LogIn = () => (
-    <form class="auth-form standard">
-        <label for="name">Name: <input name="name" required></label>
-        <label for="email">Email: <input type="email" name="email" required></label>      
-        <label for="password">Password: <input type="password" name="password" required>
-        </label>
-        <button class="signin-button">Sign Up</button>
-        <p class="go-back-link"><a href="../index.html">Go Back</a></p>
-    </form>
+const SignUp = () => (
+  <form className={styles.SignUp}>
+    <label htmlFor="name">Name: <input name="name" id="name" required /></label>
+    <label htmlFor="email">Email: <input type="email" id="email" name="email" required /></label>      
+    <label htmlFor="password">Password: <input type="password" id="password" name="password" required />
+    </label>
+    <button className="signin-button">Sign Up</button>
+    <p className="go-back-link"><a href="../index.html">Go Back</a></p>
+  </form>
 );
 
-export default LogIn;
+export default SignUp;
 
---
+++ src/components/services/request.js
@@ -0,0 +1,18 @@
+const request = (path, method, body) => {
+  return fetch(`https://covr-lettrs.herokuapp.com/api/v1/${path}`, {
+    method,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include',
+    body: JSON.stringify(body)
+  })
+    .then(res => Promise.all([res.ok, res.json()]))
+    .then(([ok, json]) => {
+      if(!ok) throw json;
+      return json;
+    });
+};
+
+export const post = (path, body) => request(path, 'POST', body);
+export const get = path => request(path, 'GET');
