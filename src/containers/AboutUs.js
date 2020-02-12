b1a1b9c79b2fb1c694bae2ac8239dd51ca196dde
Nikki <nikki.satopdx@gmail.com>
2020-02-11 17:39:12 -0800

    brokeh image

--- src/containers/AboutUs.js
+++ src/containers/AboutUs.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import About from '../components/About/About';
-
+import styles from './AboutUs.css';
 
 const AboutUs = () => (
-  <About />
+  <main className={styles.main}>
+    <About />
+  </main>
 );
 
 export default AboutUs;
