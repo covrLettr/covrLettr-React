e67b48bfa4d6d41de1fd81b073f69a23ae290f93
Travis Molter <travismolter@gmail.com>
2020-02-11 16:12:14 -0800

    connected our SignUp componenent with our hooks

--- src/components/LogUpS/SignUp.js
+++ src/components/LogUpS/SignUp.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './LogUpS.css';
+import { useSignup } from '../hooks/auth';
 
-const SignUp = () => (
-  <form className={styles.SignUp}>
-    <label htmlFor="name">Name: <input name="name" id="name" required /></label>
-    <label htmlFor="email">Email: <input type="email" id="email" name="email" required /></label>      
-    <label htmlFor="password">Password: <input type="password" id="password" name="password" required />
-    </label>
-    <button className="signin-button">Sign Up</button>
-    <p className="go-back-link"><a href="../index.html">Go Back</a></p>
-  </form>
-);
+const SignUp = () => {
+  const { signup, authErr } = useSignup();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  
+  const handleSubmit = event => {
+    event.preventDefault();
+    signup(email, password);
+  };
+
+  return (
+    <>
+      {authErr && <p>{authErr}</p>}
+      <form className={styles.SignUp} onSubmit={handleSubmit}>
+        <label htmlFor="email">Email: <input type="text" onChange={({ target }) => setEmail(target.value)} id="email" name="email" required /></label>      
+        <label htmlFor="password">Password: <input type="password" id="password" name="password" onChange={({ target }) => setPassword(target.value)} required />
+        </label>
+        <button className="signin-button">Sign Up</button>
+        <p className="go-back-link"><a href="../index.html">Go Back</a></p>
+      </form>
+    </>
+  );
+};
 
 export default SignUp;
 
