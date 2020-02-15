b7077f53f0c364d7895d4ba509abbe4db58e1236
TravelFiend <mikegrace274@gmail.com>
2020-02-15 14:24:24 -0800

    fixed merge conflicts

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -13,17 +13,11 @@ const Form = ({ onSubmit }) => {
     <section className={styles.Form}>
       <form onSubmit={onSubmit}>
         {inputFormElements}
-<<<<<<< HEAD
-        <input type="reset"  name="ResetButton" value="Reset"></input>
-        <button> Create </button>
-=======
         <button onSubmit={onSubmit} > Create </button>
         <input type="reset" className="reset" name="ResetButton" value="Reset"></input>
->>>>>>> d8455da4f27e79a59b35cb52c4c9d85ec353dd23
       </form>
     </section>
   );
-    
 };
 
 Form.propTypes = {
