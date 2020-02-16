7c733fa122b8ce451fab312b1d373abcdc2c01bb
TravelFiend <mikegrace274@gmail.com>
2020-02-15 16:55:44 -0800

    steps towards posting answers

--- src/components/InputForm/InputForm.js
+++ src/components/InputForm/InputForm.js
@@ -2,17 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './InputForm.css';
 
-const InputForm = ({ nameOfQuestion, placeholder, question }) => (
+const InputForm = ({ handleChange, nameOfQuestion, placeholder, question }) => (
   <>
     <label className={styles.InputForm} htmlFor={nameOfQuestion} >{question}</label>
-    <input type="text" name={nameOfQuestion} placeholder={placeholder} id={nameOfQuestion} />
+    <input onChange={handleChange} type="text" name={nameOfQuestion} placeholder={placeholder} id={nameOfQuestion} />
   </>
 );
 
 InputForm.propTypes = {
   nameOfQuestion: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
-  question: PropTypes.string.isRequired
+  question: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired
 };
 
 export default InputForm;
--- src/containers/CovrLettr.js
+++ src/containers/CovrLettr.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from '../components/Form/Form';
-// import HeroImage from '../components/HeroImage/HeroImage';
 import styles from './CovrLettr.css';
+import get from '../components/services/request';
 
-const CovrLettr = () => (
-  <main className={styles.Main}>
-    <div>
-      <Form />
-    </div>
-  </main>
-);
+const CovrLettr = () => {
+  const [userAnswers, setUserAnswers] = useState({});
+
+  const handleChange = ({ target }) => {
+    return setUserAnswers({ [target.name]: target.value });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    console.log('pooooooooop');
+    
+  };
+
+  return (
+    <main className={styles.Main}>
+      <div>
+        <Form handleSubmit={handleSubmit} handleChange={handleChange} />
+      </div>
+    </main>
+  );
+};
 export default CovrLettr;
