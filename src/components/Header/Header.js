4da7dee44c81d4dcce124dd76fcda11c18b6893f
treem0 <travismolter@gmail.com>
2020-08-20 14:55:28 -0700

    created logout button when user is logged in. with MIKEG

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.css';
+import { useHasSession } from '../hooks/auth';
 
-const Header = () => (
-  <header className={styles.Header}>
-    <h1>CovrLettr</h1>
-    <nav>
-      <Link to="/">
-        <span>Home</span>
-      </Link>
+const Header = () => {
+  const user = useHasSession();
 
-      <Link to="/userauth">
-        <span>Signup/Login</span>
-      </Link>
 
-      <Link to="/about">
-        <span>About</span>
-      </Link>
-    </nav>
-  </header>
-);
+  return (
+    <header className={styles.Header}>
+      <h1>CovrLettr</h1>
+      <nav>
+        <Link to="/">
+          <span>Home</span>
+        </Link>
+
+        {!user ? <Link to="/userauth">
+          <span>Signup/Login</span>
+        </Link> : <span>Logout</span>}
+
+        <Link to="/about">
+          <span>About</span>
+        </Link>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
