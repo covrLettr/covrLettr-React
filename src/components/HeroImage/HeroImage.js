71fa3d3b1d21f26ed21846074a704dc02a30ff9d
TravelFiend <mikegrace274@gmail.com>
2020-08-24 21:28:05 -0400

    dist

--- src/components/HeroImage/HeroImage.js
+++ src/components/HeroImage/HeroImage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './HeroImage.css';
-import HeroPicture from '../assets/heroImage.jpg';
+import HeroPicture from '../../../public/assets/heroImage.jpg';
 
 const HeroImage = () => <img className={styles.img} src={HeroPicture} />;
 
